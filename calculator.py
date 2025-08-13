318fa4143e60dc7fa6f585b014be0929ab3be019
jalwa dhanish <jalwadhanish2@gmail.com>
2025-08-13 10:58:15 +0530

    calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 # Basic calculator
 a = 10
 b = 20
+print ("The addition of two number is: ", a + b)
 print ("The substration of two number is: ", a - b)
+print ("The multiplication of two number is: ", a * b)
+print ("The devision of two number is: ", a + b)
